c621376649ac80f341253df9d9b17c600189ea43
fcaraengineer <fabrizio.cara@unica.it>
2020-08-28 12:43:05 +0200

    Home Page upload

--- static/js/home.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    var homeButton = document.getElementById("home_button");
-    homeButton.classList.add('active');
-});
--- static/js/research.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function() {
-    var researchButton = document.getElementById("research_button");
-    researchButton.classList.add('active');
-});
