57ac5f327e20ced6c0ddfefbb2960406e6ef5c40
fcaraengineer <fabrizio.cara@unica.it>
2020-08-28 14:12:26 +0200

    Contact Page Upload

--- static/js/index.js
+++ static/js/index.js
@@ -10,7 +10,7 @@ function resizePage(){
     if ($(window).height() >= 790) {
         var windowHeight = $(window).height();
         var navbarHeight = $(".navbar").height();
-        var finalPageHeight = windowHeight - navbarHeight;
+        var finalPageHeight = windowHeight - navbarHeight - 10;
         document.getElementById('page_content').style.height = finalPageHeight + "px";
     }
     else {
